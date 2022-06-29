e58e3b017f7bc0d0bdf4663ee5f3369c8a90357f
Patricia Ong <patricia.ong977@gmail.com>
2022-06-28 21:00:27 -0500

    Finished Day 1.

---
+++ Day 1/day1.py
@@ -0,0 +1,15 @@
+# Input Function and Print Function. print(), input()
+print("Hello World " + input("Insert your name: "))
+
+# Length of a string. len()
+print(len(input("What is your name? ")))
+
+# Variables
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+name = "Angela" # Will overwrite the previous variable. This is called reassignment.
+print(name)
+
+
