711d976da53bf4f1e7f1b818f068b58c115ac21f
Patricia Ong <patricia.ong977@gmail.com>
2022-07-06 13:35:00 -0500

    Updating Day 5 content.

--- Interactive Coding Exercises/Day 5/addingEvens.py
+++ Interactive Coding Exercises/Day 5/addingEvens.py
@@ -1,9 +1,17 @@
-#Write your code below this row 👇
+# Write your code below this row 👇
 
-even_Sum = 0 # Store the sum of the even numbers.
+even_Sum = 0  # Store the sum of the even numbers.
 
 # Use range for a list of 1 - 100
 for num in range(2, 101, 2):  # Start from 2 and skip by 2's.
-    even_Sum += num # Add the number to the sum.
+    even_Sum += num  # Add the number to the sum.
 
-print(even_Sum) # Should print 2550.
+print(even_Sum)  # Should print 2550.
+
+
+# Another solution: Using modulo (%).
+# total = 0
+# for num in range(1, 101):
+#   if num % 2 == 0: # If the remainder of num / 2 is 0,
+#     total += num
+# print(total)
