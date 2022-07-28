074034506c5c3427e5c80eb0e32443d0d4c2c8bd
patriciaong977 <patricia.ong977@gmail.com>
2022-07-27 17:42:02 -0700

    Updated Day 7 Hangman Project

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -1,4 +1,4 @@
-# Hangman
-
+# Hangman -- Check Mini Projects for Day 7.
+#
 # Flow chart Programming
 # In Handwritten Notes, there is a hangman file.
--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -1,38 +1,43 @@
 import random
 
-#Step 1
-
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
-#Testing code
+# Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-
 display = []  # Create an empty List called display.
-length_Of_Word = len(chosen_word) # Need the length of word chosen to make the range.
+# Need the length of word chosen to make the range.
+length_Of_Word = len(chosen_word)
 
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-for eachLetter in range(length_Of_Word): # for each letter in the range of the length of the word.
-  display += "_"
-
+# For each letter in the chosen_word, add a "_" to 'display'.
+# for each letter in the range of the length of the word.
+for eachLetter in range(length_Of_Word):
+    display += "_"
 print(display)
 
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
+# Use a while loop to keep asking the user for a guess until they guess the correct word.
+game_over = False  # Use a variable to keep track if the game is over or not.
 
-# Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+while not game_over:
+    # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    guess = input("Guess a letter: ").lower()
 
-for pos in range(length_Of_Word): # for each position in the length of the word
-  letter = chosen_word[pos] # Save the value/letter to the var.
-  if letter == guess: # if letter == guess
-    display[pos] = letter # Replace the "_" with the actual letter
+    # Check if the guess is in the chosen_word.
+    # for each position in the length of the word.
+    for pos in range(length_Of_Word):
+        letter = chosen_word[pos]  # Save the value/letter to the var.
+        if letter == guess:  # if letter == guess
+            display[pos] = letter  # Replace the "_" with the actual letter.
+        # Print 'display'. Show correctly guessed letters and the remaining "_"
+    print(display)
 
+    # Check if there are no more "_" in the display.
+    if "_" not in display:
+        game_over = True
+        print("You win!")
 
 
 # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
@@ -43,7 +48,3 @@ for pos in range(length_Of_Word): # for each position in the length of the word
 #         print("Correct!")
 #     else:
 #         print("Incorrect!")
-
-#Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
