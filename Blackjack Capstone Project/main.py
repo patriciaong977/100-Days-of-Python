07127d98b359ef5534848191749f8d952d2ffbee
patriciaong977 <patricia.ong977@gmail.com>
2022-08-22 18:25:25 -0500

    Finished Blackjack Project

--- Blackjack Capstone Project/main.py
+++ Blackjack Capstone Project/main.py
@@ -15,14 +15,18 @@
 ### Imported Libraries ###
 from art import logo  # Import logo from art.py
 import random # Import random library. (To choose randomly from deck of cards)
-
-
-### Variables ###
-player_cards = []  # Create a list to hold the player's cards.
-dealer_cards = []  # Create a list to hold the dealer's cards.
+from os import system, name # Create clear function. (To clear the screen)
 
 
 ### Functions ###
+def clear():
+  """ Clears the screen."""
+  if name == 'nt':    # For windows.
+    _ = system('cls')
+  else:               # For Unix and Linux
+    _ = system('clear')
+
+
 def deal_card():
   """ Deals a random card from the list of cards in the deck.
 
@@ -32,6 +36,7 @@ def deal_card():
   deck_of_Cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] # Create the deck of cards. Represent Ace with 11, 10/Jack/Queen/King with 10.
   random_Card = random.choice(deck_of_Cards)  # Choose a random card from the deck of cards.
   return random_Card
+
 
 def calculate_Hand(list_of_Cards):
   """ Takes a list of cards as the input and returns the sum of the Hand.
@@ -45,12 +50,9 @@ def calculate_Hand(list_of_Cards):
     hand_Sum (int) : Returns the sum of the list of cards.
   """
   hand_Sum = 0 # Create a variable to hold the sum of the Hand.
+  hand_Sum = sum(list_of_Cards) # Sum the list of cards.
 
-  # Sum the list of cards.
-  for card in list_of_Cards: # For each card in the list of cards...
-    hand_Sum += card # Add the card to the sum of the Hand.
-
-  # Check the hand_Sum for a blackjack.
+  # Check the hand_Sum for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score.
   if hand_Sum == 21 and len(list_of_Cards) == 2:
     return 0
 
@@ -58,22 +60,77 @@ def calculate_Hand(list_of_Cards):
   if 11 in list_of_Cards and hand_Sum > 21:
     list_of_Cards.remove(11)
     list_of_Cards.append(1)
-
   return hand_Sum
 
 
-# For loop to deal the player and dealer 2 cards each.
-for i in range(2):
-  player_cards.append(deal_card()) # Add the random card to the player's cards list.
-  dealer_cards.append(deal_card()) # Add the random card to the dealer's cards list.
+def compare(player_Score, dealer_Score):
+  """ Compares the player's Score and the dealer's Score and returns the winner.
+      Returns a string with the result of the comparison.
+  """
+  if player_Score == dealer_Score:
+    return "\nIt's a tie!\n"
+  elif player_Score == 0:
+    return "\nBlackjack! \nYou win!\n"
+  elif dealer_Score == 0:
+    return "\nBlackjack! \nThe dealer wins!\n"
+  elif player_Score > 21:
+    return "\nYou busted! \nThe dealer wins!\n"
+  elif dealer_Score > 21:
+    return "\nThe dealer busted! \nYou win!\n"
+  elif player_Score > dealer_Score:
+    return "\nYou win!"
+  else:
+    return "\nSorry you lose, the dealer wins!\n"
 
-# Checking
-print(player_cards)
-print(dealer_cards)
+def playGame():
+  print(logo) # Display logo
 
-calculate_Hand(player_cards) # Call the function to calculate the score of the player and dealer.
 
-if calculate_Hand() == 0: # If the player has a blackjack...
-  print("Blackjack! You win!") # Print "Blackjack! You win!"
-else:
-  print("You lose!") # Else, print "You lose!"
+  ### Variables ###
+  player_cards = []  # Create a list to hold the player's cards.
+  dealer_cards = []  # Create a list to hold the dealer's cards.
+  is_game_over = False  # Create a variable to hold the game status.
+
+  # For loop to deal the player and dealer 2 cards each.
+  for i in range(2):
+    player_cards.append(deal_card()) # Add the random card to the player's cards list.
+    dealer_cards.append(deal_card()) # Add the random card to the dealer's cards list.
+
+  # User Move
+  while not is_game_over:
+    # Call the calculate_Hand function to calculate the sum of the player's and dealer's cards.
+    player_Score = calculate_Hand(player_cards)
+    dealer_Score = calculate_Hand(dealer_cards)
+
+    # Print the player's and dealer's cards and their scores.
+    print(f"Your cards: {player_cards} , Your current score: {player_Score}")
+    print(f"Dealer's first card: {dealer_cards[0]} \n")
+
+    # If the player or dealer has a blackjack (0) or the player has 21, the game is over.
+    if player_Score == 0 or dealer_Score == 0 or player_Score > 21:
+      is_game_over = True
+    else:  # If the game is not over, ask the player if they want to hit or stay.
+      player_Input = input(
+          "Type 'hit' to draw another card, type 'stay' to pass: ")
+      if player_Input == "hit":
+        # Add the random card to the player's cards list.
+        player_cards.append(deal_card())
+      else:
+        is_game_over = True
+
+  # Dealer Move -- Dealer will draw a card until their score is less than 17.
+  while dealer_Score != 0 and dealer_Score < 17:
+    dealer_cards.append(deal_card()) # Add the random card to the dealer's cards list.
+    dealer_Score = calculate_Hand(dealer_cards) # Calculate the sum of the dealer's cards.
+
+  print (f"\nYour final hand: {player_cards} , Your final score: {player_Score}")
+  print (f"Dealer's final hand: {dealer_cards} , Dealer's final score: {dealer_Score}")
+
+  # Call the compare function to compare the player's and dealer's scores and print the result.
+  print(compare(player_Score, dealer_Score))
+
+
+# Play again?
+while input ("Do you want to play BlackJack? Type 'y' or 'n': ") == "y":
+  clear()
+  playGame()
