791b31dbfdd65369d6ea60128fff648e5fdcc74f
patriciaong977 <patricia.ong977@gmail.com>
2022-11-16 19:33:45 -0600

    Need to debug

--- Mini Projects/coffeeMachine/main.py
+++ Mini Projects/coffeeMachine/main.py
@@ -2,11 +2,42 @@
 from menuResources import MENU, resources
 
 
+# CHECK resources sufficient.
+def check_resource_sufficient(order_ingredients):
+    """ Return true if the resources are sufficient, false if not. """
+    for i in order_ingredients:
+        if order_ingredients[i] >= resources[i]:
+            print(f"Sorry there is not enough {i}.")
+            return False
 
+# PROCESS coins.
+def process_coins():
+    """ Return the total calculated from the coins inputted. """
+    print("Insert amount of coins.")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.10
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
 
-# TODO: PROCESS coins.
-# TODO: CHECK transaction successful.
-# TODO: MAKE COFFEE.
+# CHECK transaction successful.
+def check_transaction(total_coins, drink_cost):
+    """ Return true if the payment is accepted, false if not. """
+    if total_coins >= drink_cost:
+        change = round(total_coins - drink_cost, 2) # Round the change to 2 decimal places.
+        print(f"Here is your change: ${change}")
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+# MAKE COFFEE.
+def make_coffee(drink_name, order_ingredients):
+    """ Use the ingredients to make the coffee. """
+    for i in order_ingredients:
+        resources[i] -= order_ingredients[i]
+    print(f"Here is your {drink_name} ☕. Enjoy!")
+
 
 IS_ON = True
 
@@ -21,8 +52,11 @@ while IS_ON: # while True:
     elif choice == "report":  # PRINT REPORT by entering "report" to the prompt.
         print(f"Water: {resources['water']}ml")
         print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
+        print(f"Coffee: {resources['coffee']}")
         print(f"Money: ${resources['money']}")
     else:
         drink = MENU[choice]
-        # CHECK resources sufficient.
+        if check_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if check_transaction(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
