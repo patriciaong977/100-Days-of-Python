e6650a3e7f4b465f3aae2b24f28584a70254c65e
patriciaong977 <patricia.ong977@gmail.com>
2023-01-13 15:00:16 -0600

    Working on Tuples.

---
+++ Day 18/tuples.py
@@ -0,0 +1,30 @@
+# Python Tuples
+'''
+Syntax - my_tuple = (1,3,8)
+Tuples are ordered.
+Immutable - can't change.
+Can convert tuple into a list by list(my_tuple)
+
+https://www.w3schools.com/colors/colors_rgb.asp
+'''
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+# colours = ["salmon", "dark orange", "RoyalBlue1", "Chartreuse2",
+#           "goldenrod", "dark turquoise", "olive drab", "DeepSkyBlue2"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+# for _ in range(200):
+#     tim.color(random.choice(colours))
+#     tim.forward(20)
+#     tim.setheading(random.choice(directions))
--- Day 18/turtleMove.py
+++ Day 18/turtleMove.py
@@ -7,8 +7,8 @@ turtle_Point = Turtle()
 turtle_Point.shape("turtle")
 # turtle_Point.color("pale green")
 # turtle_Point.pencolor("dark green")
-turtle_Point.turtlesize(2,2,1) # (wide, len, outline)
-turtle_Point.speed(1)
+turtle_Point.turtlesize(1.5,1.5,1.5) # (wide, len, outline)
+# turtle_Point.speed(1)
 
 # Movement
 # # Draw a Square
@@ -26,20 +26,42 @@ turtle_Point.speed(1)
 #     turtle_Point.penup()  # Drawing when moving.
 #     turtle_Point.forward(10)
 
-# Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, and decagon. (8 shapes)
-  # Each shape is drawn with a random color.
-  # Each sides are going to be 100 in terms of length.
-  # 360 / num of sides.
-colors = ["salmon", "dark orange", "RoyalBlue1","Chartreuse2",
+# # Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, and decagon. (8 shapes)
+#   # Each shape is drawn with a random color. https://trinket.io/docs/colors
+#   # Each sides are going to be 100 in terms of length.
+#   # 360 / num of sides.
+# colors = ["salmon", "dark orange", "RoyalBlue1","Chartreuse2",
+#           "goldenrod", "dark turquoise", "olive drab", "DeepSkyBlue2"]
+
+# for sides in range(3, 11): # 11 - stop number is included. Will stop at 10.
+#     turtle_Point.color(random.choice(colors))   # Choose a random color from list.
+
+#     for _ in range(sides):  #  360 / 3 = 120, 360 / 4 = 90 ...
+#         turtle_Point.forward(100)
+#         turtle_Point.right(360 / sides)
+
+# Draw a Random Walk.
+  # Turtle making a random walk (north, south, east, west).\
+  # Increase thickness.
+  # Increase speed.
+  #
+
+colors = ["salmon", "dark orange", "RoyalBlue1", "Chartreuse2",
           "goldenrod", "dark turquoise", "olive drab", "DeepSkyBlue2"]
 
-for sides in range(3, 10):
-    turtle_Point.color(random.choice(colors))   # Choose a random color from list.
+# East = 0, North = 90, West = 180, South = 270
+direction = [0, 90, 180, 270]
 
-    for i in range(sides):
-        turtle_Point.forward(100)
-        turtle_Point.right(360/sides)
+turtle_Point.speed(10)
+turtle_Point.pensize(8)
 
+# Movement
+for _ in range(100):
+    turtle_Point.color(random.choice(colors)) # Change colors
+
+    # Set orientation of the turtle. (Use angles)
+    turtle_Point.setheading(random.choice(direction))
+    turtle_Point.forward(20)
 
 # After execution of above.
 screen = Screen()
